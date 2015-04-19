0265e09a40fc31ef93fe8278c12c277484cd4970
Yuichi Murata <mrk21info+github@gmail.com>
2015-04-19 20:01:06 +0900

    Fixed the job spec(rails_active_job).

--- rails_active_job/spec/jobs/my_job_spec.rb
+++ rails_active_job/spec/jobs/my_job_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe MyJob, type: :job do
   describe 'perform(message)' do
-    it 'should invoke Task#exec(message)' do
-      allow_any_instance_of(Task).to receive(:exec)
-      MyJob.perform_later('Task exec.')
+    it 'should invoke Task#exec() with the message' do
+      message = 'hoge'
+      expect_any_instance_of(Task).to receive(:exec).with(message)
+      MyJob.new.perform(message)
     end
   end
 end
