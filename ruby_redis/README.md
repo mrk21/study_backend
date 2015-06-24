dcaa810ecb3e94bf2f8618e2bd1072ecf53baf7a
Yuichi Murata <mrk21info+github@gmail.com>
2015-06-24 11:15:53 +0900

    Added the test script of Server Side Lua to the ruby_redis.

--- ruby_redis/README.md
+++ ruby_redis/README.md
@@ -28,3 +28,8 @@ If you want to run record collection test script, you take steps listed below:
 * [redisドキュメント日本語訳 — redis 2.0.3 documentation](http://redis.shibu.jp/)
 * [RedisをRubyから触ってみた - ごろねこ日記](http://d.hatena.ne.jp/hiroe_orz17/20111006/1317890657)
 * [Redisのサイズの測り方 - ゆっくり備忘録](http://mitsuakikawamorita.com/blog/?p=1823)
+
+### Server Side Lua
+
+* [RedisのLuaスクリプティング機能について « Rest Term](http://rest-term.com/archives/3038/)
+* [More Transactional Redis (2) - Lua Scripting in Action - 愛と勇気と缶ビール](http://zentoo.hatenablog.com/entry/20130718/1374152848)
---
+++ ruby_redis/lua.rb
@@ -0,0 +1,31 @@
+require 'pp'
+require 'bundler'
+Bundler.require
+
+redis = Redis.new
+redis.select 1
+
+# Redis will raise `Redis::CommandError` exception shown below when occurred an error at `redis.call()` method of Server Side Lua:
+# #<Redis::CommandError: ERR Error running script (call to f_e4cbd2c8118199ca36271d07d14b33c6505226d7): @user_script:1: @user_script: 1: Unknown Redis command called from Lua script>
+begin
+  pp redis.eval(<<-LUA)
+    return redis.call('dddd')
+  LUA
+rescue => e
+  pp e
+end
+
+# Server Side Lua provided by Redis can catch an error occurring at `redis.call()` method by using `pcall()` method as same as normal Lua script:
+# [nil, "=[C]: -1: Unknown Redis command called from Lua script"]
+pp redis.eval(<<-LUA)
+  local ok, error = pcall(redis.call, 'dddd')
+  return {ok, error}
+LUA
+
+# The `eval` command can specify arguments that are `KEYS` and `ARGV`.
+# Both does not differ basically, but you need to use properly the both arguments in some case.
+# In the detail information of this matter, refer to the link listed below:
+# @see RedisのLua ScriptingのKEYSとARGVの違いについて調査 - ゆっくり備忘録, http://mitsuakikawamorita.com/blog/?p=2621
+pp redis.eval(<<-LUA, keys: [:key1, :key2], argv: [:arg1, :arg2]) # [["key1", "key2"], ["arg1", "arg2"]]
+  return {KEYS, ARGV}
+LUA
