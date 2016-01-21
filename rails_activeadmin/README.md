6811b9b826412fc0576367c8ac3372ad14f230f4
Yuichi Murata <mrk21info+github@gmail.com>
2016-01-21 23:26:16 +0900

    rails_activeadmin: add a resource

--- rails_activeadmin/README.md
+++ rails_activeadmin/README.md
@@ -27,3 +27,4 @@ You enter the credential listed below:
 * [Active Admin | The administration framework for Ruby on Rails](http://activeadmin.info/docs/documentation.html)
 * [Ruby - Rails 4 ＆ Active Admin で、マスタCRUD系の管理画面を秒速で作る方法 - Qiita](http://qiita.com/hkusu/items/3b0fb7f94a254e2ed6fd)
 * [ActiveAdmin 管理画面でモデルデータを見る | Rails / TakayukiKoyama.com](http://rails.takayukikoyama.com/gem/activeadmin/)
+* [Active AdminでActiveModel::ForbiddenAttributesError Ruby on RailsでWebサイト公開！に挑戦中](http://hbnist76.blog.fc2.com/blog-entry-476.html)
--- rails_activeadmin/db/schema.rb
+++ rails_activeadmin/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160120001942) do
+ActiveRecord::Schema.define(version: 20160121141912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,5 +48,12 @@ ActiveRecord::Schema.define(version: 20160120001942) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
