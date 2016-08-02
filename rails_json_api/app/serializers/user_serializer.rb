8af6bc7c6f693a21ea33970c1b967c5bc7ecf7b3
Yuichi Murata <mrk21info+github@gmail.com>
2016-08-02 22:44:15 +0900

    rails_json_api: add a code of meta

--- rails_json_api/app/serializers/user_serializer.rb
+++ rails_json_api/app/serializers/user_serializer.rb
@@ -1,7 +1,15 @@
 class UserSerializer < ActiveModel::Serializer
-  has_many :notes
+  has_many :notes do
+    meta do
+      { aa: 1 }
+    end
+  end
   
   attributes :id
   attributes :username
   attributes :email
+
+  meta do
+    { aa: 1 }
+  end
 end
