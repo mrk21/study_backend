72032b943813cbee2028a50b47717a4843ea99e6
Yuichi Murata <mrk21info+github@gmail.com>
2017-01-25 13:13:10 +0900

    ruby_graphql_test: add

---
+++ ruby_graphql_test/README.md
@@ -0,0 +1,12 @@
+# Ruby GraphQL test
+
+## How to start
+
+1. bundle install
+2. bundle exec ruby test.rb
+
+## Refer to
+
+* [Introduction to GraphQL | GraphQL](http://graphql.org/learn/)
+* [GraphQL Ruby - Welcome](https://rmosolgo.github.io/graphql-ruby/)
+* [西日暮里.rbでGraphQL on RubyというLTをした - Please Drive Faster](http://joe-re.hatenablog.com/entry/2016/05/04/174742)
---
+++ ruby_graphql_test/test.rb
@@ -0,0 +1,77 @@
+require 'bundler'
+Bundler.require
+require 'pp'
+
+class Comment < Struct.new(:id, :body, :created_at)
+end
+
+class Post < Struct.new(:id, :title, :body, :comments)
+  class << self
+    def all
+      [
+        self.new('1', 'title 1', 'body 1', [Comment.new('1', 'comment 1', Time.now)]),
+        self.new('2', 'title 2', 'body 2', [Comment.new('2', 'comment 2', Time.now)]),
+        self.new('3', 'title 3', 'body 3', [Comment.new('3', 'comment 3', Time.now)]),
+      ]
+    end
+
+    def find(id)
+      all.find { |record| record.id == id }
+    end
+  end
+end
+
+PostType = GraphQL::ObjectType.define do
+  name "Post"
+  description "A blog post"
+  # `!` marks a field as "non-null"
+  field :id, !types.ID
+  field :title, !types.String
+  field :body, !types.String
+  field :comments, types[!CommentType]
+end
+
+CommentType = GraphQL::ObjectType.define do
+  name "Comment"
+  field :id, !types.ID
+  field :body, !types.String
+  field :created_at, !types.String
+end
+
+QueryType = GraphQL::ObjectType.define do
+  name "Query"
+  description "The query root of this schema"
+
+  field :post do
+    type PostType
+    argument :id, !types.ID
+    description "Find a Post by ID"
+    resolve ->(obj, args, ctx) { Post.find(args["id"]) }
+  end
+end
+
+Schema = GraphQL::Schema.define do
+  query QueryType
+end
+
+
+# {
+#   "data"=> {
+#     "post"=> {
+#       "id"=>"1",
+#       "title"=>"title 1",
+#       "comments"=> [
+#         {"body"=>"comment 1", "created_at"=>"%Y-%m-%d %H:%M:%S %z"}
+#       ]
+#     }
+#   }
+# }
+pp Schema.execute <<-GRAPHQL
+  {
+    post(id: 1) {
+      id
+      title
+      comments { body created_at }
+    }
+  }
+GRAPHQL
