85c163ca5b84ba9e4a57fe3f90ad47790c40e9dc
Yuichi Murata <mrk21info+github@gmail.com>
2017-02-06 19:11:52 +0900

    rails_graphql: add a code of test

--- rails_graphql/spec/requests/posts_spec.rb
+++ rails_graphql/spec/requests/posts_spec.rb
@@ -1,33 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
-  describe "query post(id: ID!)" do
-    let!(:record) { FactoryGirl.create :post }
-    let!(:current_user) { record.user }
+  let!(:record) { FactoryGirl.create :post }
+  let!(:current_user) { record.user }
 
-    before do
-      query =<<-GQ
-        mutation CreateSession($input: CreateSessionInput!) {
-          createSession(input: $input) {
-            user {
-              name
-            }
+  before do
+    query =<<-GQ
+      mutation CreateSession($input: CreateSessionInput!) {
+        createSession(input: $input) {
+          user {
+            name
           }
         }
-      GQ
-      variables = {
-        input: {
-          email: current_user.email,
-          password: current_user.password
-        }
       }
-      post queries_path, query: query, variables: variables
-    end
+    GQ
+    variables = {
+      input: {
+        email: current_user.email,
+        password: current_user.password
+      }
+    }
+    post queries_path, params: {
+      query: query,
+      variables: variables
+    }
+  end
 
+  describe "query post(id: ID!)" do
     let(:query) {
       <<-GRAPHQL
         query {
-          post(id: #{record.id}) {
+          post(id: "#{record.id}") {
             id
             title
             body
@@ -39,7 +42,7 @@ RSpec.describe "Posts", type: :request do
     }
 
     subject {
-      post queries_path, query: query
+      post queries_path, params: { query: query }
       response.body
     }
 
@@ -52,6 +55,49 @@ RSpec.describe "Posts", type: :request do
             body: String,
             createdAt: String,
             updatedAt: String,
+          }
+        }
+      })
+    end
+  end
+
+  describe "query posts" do
+    let(:query) {
+      <<-GRAPHQL
+        query {
+          posts {
+            edges {
+              node {
+                id
+                title
+                body
+                createdAt
+                updatedAt
+              }
+            }
+          }
+        }
+      GRAPHQL
+    }
+
+    subject {
+      post queries_path, params: { query: query }
+      response.body
+    }
+
+    it 'returns posts' do
+      is_expected.to be_json_as({
+        data: {
+          posts: {
+            edges: [{
+              node: {
+                id: String,
+                title: String,
+                body: String,
+                createdAt: String,
+                updatedAt: String,
+              }
+            }]
           }
         }
       })
