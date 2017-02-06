a1f46735e0f86ce5e59e4ad1d6244a2e0424b270
Yuichi Murata <mrk21info+github@gmail.com>
2017-02-06 19:37:03 +0900

    rails_graphql: add a code of test

--- rails_graphql/spec/requests/posts_spec.rb
+++ rails_graphql/spec/requests/posts_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
-  let!(:record) { FactoryGirl.create :post }
-  let!(:current_user) { record.user }
+  let!(:current_user) { FactoryGirl.create :user }
 
   before do
     query =<<-GQ
@@ -27,6 +26,8 @@ RSpec.describe "Posts", type: :request do
   end
 
   describe "query post(id: ID!)" do
+    let!(:record) { FactoryGirl.create :post, user: current_user }
+
     let(:query) {
       <<-GRAPHQL
         query {
@@ -50,11 +51,11 @@ RSpec.describe "Posts", type: :request do
       is_expected.to be_json_as({
         data: {
           post: {
-            id: String,
-            title: String,
-            body: String,
-            createdAt: String,
-            updatedAt: String,
+            id: record.id.to_s,
+            title: record.title,
+            body: record.body,
+            createdAt: record.created_at.to_s,
+            updatedAt: record.updated_at.to_s,
           }
         }
       })
@@ -62,6 +63,9 @@ RSpec.describe "Posts", type: :request do
   end
 
   describe "query posts" do
+    let!(:record1) { FactoryGirl.create :post, user: current_user }
+    let!(:record2) { FactoryGirl.create :post, user: current_user }
+
     let(:query) {
       <<-GRAPHQL
         query {
@@ -89,15 +93,26 @@ RSpec.describe "Posts", type: :request do
       is_expected.to be_json_as({
         data: {
           posts: {
-            edges: [{
-              node: {
-                id: String,
-                title: String,
-                body: String,
-                createdAt: String,
-                updatedAt: String,
+            edges: [
+              {
+                node: {
+                  id: record1.id.to_s,
+                  title: record1.title,
+                  body: record1.body,
+                  createdAt: record1.created_at.to_s,
+                  updatedAt: record1.updated_at.to_s,
+                }
+              },
+              {
+                node: {
+                  id: record2.id.to_s,
+                  title: record2.title,
+                  body: record2.body,
+                  createdAt: record2.created_at.to_s,
+                  updatedAt: record2.updated_at.to_s,
+                }
               }
-            }]
+            ]
           }
         }
       })
